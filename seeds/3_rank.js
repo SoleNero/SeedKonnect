3d1213d8f835d41c2c7e8e599d894316e585ed4c
Justyna Foster <justynawfoster@gmail.com>
2017-02-28 19:10:00 -0700

    rank seeded

--- seeds/3_rank.js
+++ seeds/3_rank.js
@@ -1,13 +1,22 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('rank').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('rank').insert([
+         {id: 1, 
+         rank: 0},
+         {id: 2, 
+         rank: 1},
+         {id: 3, 
+         rank: 2},
+         {id: 4, 
+         rank: 3},
+         {id: 5, 
+         rank: 4},
+         {id: 6, 
+         rank: 5}
       ]);
     });
 };
