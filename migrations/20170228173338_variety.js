3cf5fe514d4cd197111efde336a24caab1f27c14
Justyna Foster <justynawfoster@gmail.com>
2017-03-01 12:00:44 -0700

    added info to variety

--- migrations/20170228173338_variety.js
+++ migrations/20170228173338_variety.js
@@ -4,6 +4,9 @@ exports.up = function(knex, Promise) {
      table.string('variety_name').notNullable();
      table.integer('species_id').notNullable().references('id').inTable('species').onDelete('CASCADE');
      table.string('variety_image').notNullable().defaultTo('No photo available');
+     table.string('fruit_color').notNullable();
+     table.string('size').notNullable();
+     table.integer('maturity_days').notNullable();
    })
 };
 
