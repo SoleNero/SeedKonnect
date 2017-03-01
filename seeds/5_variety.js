5ee6e8602d3b15ffdc4f9142aa1394d6f66b32e3
Justyna Foster <justynawfoster@gmail.com>
2017-03-01 12:01:01 -0700

    added info to variety

--- seeds/5_variety.js
+++ seeds/5_variety.js
@@ -6,167 +6,290 @@ exports.seed = function(knex, Promise) {
       return knex('variety').insert([
         {id: 1, 
          variety_name: 'Amish Gold',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'medium/large',
+         maturity_days: 85
         },
         {id: 2, 
          variety_name: 'Baller',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'small',
+         maturity_days: 71
         },
         {id: 3, 
          variety_name: 'Berwick German',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'medium',
+         maturity_days: 85
         },
         {id: 4, 
          variety_name: 'Big Rainbow',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'orange yellow',
+         size: 'large',
+         maturity_days: 82
         },
         {id: 5, 
          variety_name: 'Black Ethiopian',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'purple black',
+         size: 'small',
+         maturity_days: 81
         },
         {id: 6, 
          variety_name: 'Black Plum',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'Mahogany',
+         size: 'small',
+         maturity_days: 72
         },
         {id: 7, 
          variety_name: 'Brandywine OTV',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'pink',
+         size: 'large',
+         maturity_days: 72
         },
         {id: 8, 
          variety_name: 'Brown\'s Yellow Giant',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'Orange Yellow',
+         size: 'large',
+         maturity_days: 90
         },
         {id: 9, 
          variety_name: 'Cherokee Chocolate',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'black brown',
+         size: 'medium',
+         maturity_days: 80
         },
         {id: 10, 
          variety_name: 'Costoluto Ganovesse',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'large',
+         maturity_days: 78
         },
         {id: 11, 
          variety_name: 'Delicious',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'very large',
+         maturity_days: 77
         },
         {id: 12, 
          variety_name: 'Ed\'s Fat Plum',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'medium',
+         maturity_days: 80
         },
         {id: 13, 
          variety_name: 'Evergreen',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'green',
+         size: 'medium',
+         maturity_days: 75
         },
         {id: 14, 
          variety_name: 'Fantastic',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'medium',
+         maturity_days: 85
         },
         {id: 15, 
          variety_name: 'Flamme',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'medium/large',
+         maturity_days: 85
         },
         {id: 16, 
          variety_name: 'Flamme',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'orange',
+         size: 'small',
+         maturity_days: 70
         },
         {id: 17, 
          variety_name: 'Georgia Streak',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'gold Red',
+         size: 'large',
+         maturity_days: 90
         },
         {id: 18, 
          variety_name: 'German Johnson',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'pink',
+         size: 'large',
+         maturity_days: 80
         },
         {id: 19, 
          variety_name: 'German Strip',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'orange yellow',
+         size: 'very large',
+         maturity_days: 78
         },
         {id: 20, 
          variety_name: 'Giant Belgium',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'very large',
+         maturity_days: 75
         },
         {id: 21, 
          variety_name: 'God Love',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'pink',
+         size: 'cherry',
+         maturity_days: 75
         },
         {id: 22, 
          variety_name: 'Haidi?',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'small',
+         maturity_days:75
         },
         {id: 23, 
          variety_name: 'Hess',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'yellow',
+         size: 'large',
+         maturity_days: 85
         },
         {id: 24, 
          variety_name: 'Julia Child',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'pink',
+         size: 'small',
+         maturity_days: 85
         },
         {id: 25, 
          variety_name: 'Lida Ukrainian',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'small',
+         maturity_days: 75
         },
         {id: 26, 
          variety_name: 'Marizol BratKa',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'pink',
+         size: 'large',
+         maturity_days: 85
         },
         {id: 27, 
          variety_name: 'Marizol Gold',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'gold-red',
+         size: 'large',
+         maturity_days: 90
         },
         {id: 28, 
          variety_name: 'Marz Yellow-Red',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'yellow red',
+         size: 'medium',
+         maturity_days: 80
         },
         {id: 29, 
          variety_name: 'New Zealand Pink',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'pink',
+         size: 'large',
+         maturity_days: 90
         },
         {id: 30, 
          variety_name: 'Orange Beefsteak',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'orange yellow',
+         size: 'large',
+         maturity_days: 79
         },
         {id: 31, 
          variety_name: 'Orange Russian 117',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'gold red',
+         size: 'medium',
+         maturity_days: 85
         },
         {id: 32, 
          variety_name: 'Oxheart',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'pink',
+         size: 'large',
+         maturity_days: 80
         },
         {id: 33, 
          variety_name: 'Paul Robeson',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'purple black',
+         size: 'medium',
+         maturity_days: 74
         },
         {id: 34, 
          variety_name: 'Radiator Charlie',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red pink',
+         size: 'very large',
+         maturity_days: 80
         },
         {id: 35, 
          variety_name: 'Red Pear',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'cherry',
+         maturity_days: 70
         },
         {id: 36, 
          variety_name: 'Rivieria',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'medium',
+         maturity_days: 74
         },
         {id: 37, 
          variety_name: 'Sunset Red Horizon',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'red',
+         size: 'large',
+         maturity_days: 72
         },
         {id: 38, 
          variety_name: 'White',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'white',
+         size: 'large',
+         maturity_days: 80
         },
         {id: 39, 
          variety_name: 'Yellow Gooseberry',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'orange yellow',
+         size: 'cherry',
+         maturity_days: 75
         },
         {id: 40, 
          variety_name: 'Yellow Ruffled',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'orange yellow',
+         size: 'medium',
+         maturity_days: 80
         },
         {id: 41, 
          variety_name: 'Zapotec Pleated',
-         species_id: 1
+         species_id: 1,
+         fruit_color: 'pink red',
+         size: 'medium',
+         maturity_days: 85
         }
       ]);
     })
